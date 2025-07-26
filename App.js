5f245de3115ed4611fd121af736e7bc50845c747
Gold05-Barros <goldbarros05@email.com>
2025-07-26 23:32:21 +0100

    mudei a cor do bt

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/screen3d.png')} // imagem enviada
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>Bem-vindo ao Manda</Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => console.log('Começar')}>
+        <Text style={styles.buttonText}>Começar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +22,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  image: {
+    width: 220,
+    height: 220,
+    marginBottom: 40,
+  },
+  title: {
+    fontSize: 28,
+    color: 'back',
+    fontWeight: '600',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#F2A900',
+    paddingVertical: 14,
+    paddingHorizontal: 38,
+    borderRadius: 30,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#000000',
+    fontWeight: 'bold',
   },
 });
